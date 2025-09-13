e7408c232850bd8506582b5617dc533e9653cfd7
princekamariya <kamriyaprince@gmail.com>
2025-09-13 23:28:53 +0530

    added associations

--- src/repository/city-repository.js
+++ src/repository/city-repository.js
@@ -1,4 +1,4 @@
-const { where } = require("sequelize");
+const { where, Op } = require("sequelize");
 const { City } = require("../models/index");
 
 class CityRepository {
@@ -44,6 +44,26 @@ class CityRepository {
         try {
             const city = await City.findByPk(cityId);
             return city;
+        } catch (error) {
+            console.log("Something went wrong in the repository layer");
+            throw { error };
+        }
+    }
+
+    async getAllCities(filter) {
+        try {
+            if (filter.name) {
+                const cities = await City.findAll({
+                    where: {
+                        name: {
+                            [Op.startsWith]: filter.name,
+                        },
+                    },
+                });
+                return cities;
+            }
+            const cities = await City.findAll();
+            return cities;
         } catch (error) {
             console.log("Something went wrong in the repository layer");
             throw { error };
