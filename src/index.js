265536df5217c38541303a89a85d2a866695d8ec
princekamariya <kamriyaprince@gmail.com>
2025-09-13 18:20:03 +0530

    completed basic setup for sequelize

--- src/index.js
+++ src/index.js
@@ -1,11 +1,13 @@
-const express = require('express');
-const {PORT} = require('./config/serverConfig')
+const express = require("express");
+const { PORT } = require("./config/serverConfig");
 const setUpAndStartServer = async () => {
     const app = express();
+    app.use(express.json());
+    app.use(express.urlencoded({ extended: true }));
 
-    app.listen(3000, ()=> {
+    app.listen(3000, () => {
         console.log(`Server started on ${PORT}`);
-    })
-}
+    });
+};
 
 setUpAndStartServer();
