94e80fb68a84f9e7b20c146e8e23a62c78e49c58
princekamariya <kamriyaprince@gmail.com>
2025-09-14 16:35:46 +0530

    Added crud repo

---
+++ src/repository/airport-repository.js
@@ -0,0 +1,9 @@
+const CrudRepository = require("./crud-repository");
+
+class AirportRepository extends CrudRepository {
+    constructor(model) {
+        super(model);
+    }
+}
+
+module.exports = AirportRepository;
--- src/repository/index.js
+++ src/repository/index.js
@@ -1,9 +1,13 @@
 const AirplaneRepository = require("./airplane-repository");
+const AirportRepository = require("./airport-repository");
 const CityRepository = require("./city-repository");
+const CrudRepository = require("./crud-repository");
 const FlightRepository = require("./flight-repository");
 
 module.exports = {
     CityRepository,
     AirplaneRepository,
     FlightRepository,
+    CrudRepository,
+    AirportRepository,
 };
